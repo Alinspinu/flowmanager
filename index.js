cc07d37071b0c86290f8feae7a7e46b19c89e233
Alin Spinu <alinspinu@Alins-MacBook-Pro.local>
2023-03-09 13:00:37 +0200

    all done

--- index.js
+++ index.js
@@ -149,7 +149,7 @@ app.use((err, req, res, next) => {
   res.status(statusCode).render("error", { err });
 });
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`App running on port ${port}`);
   console.log(
